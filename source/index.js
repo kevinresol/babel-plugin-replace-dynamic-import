80d718b2b9f84170ffec5b1e6d962d044fcec12b
Kevin Leung <kevinresol@gmail.com>
2019-04-15 21:35:46 +0800

    Simple

--- source/index.js
+++ source/index.js
@@ -1,41 +1,10 @@
-import babelTemplate from 'babel-template';
-import dynamicImportSyntax from 'babel-plugin-syntax-dynamic-import';
-import * as types from 'babel-types';
-
-const IMPORT = 'Import';
-const CALL = 'CallExpression';
-
-const generateSourceNode = node => {
-  const astNode = node[0];
-  let path = types.templateLiteral(
-    [
-      types.templateElement({ raw: '' }),
-      types.templateElement({ raw: '' }, true)
-    ],
-    node,
-  );
-  if (types.isStringLiteral(astNode) || types.isTemplateLiteral(astNode)) {
-    path = node;
-  }
-  return path;
-};
-
-
-
 export default () => ({
-  inherits: dynamicImportSyntax,
   visitor: {
-    Program: {
-      enter(path, state) {
-        path.traverse({
-          [CALL]: path => {
-            if (path.node.callee.type === IMPORT) {
-              const sourcePath = generateSourceNode(path.node.arguments);
-              path.replaceWith(babelTemplate(`${state.opts.by || '__import__'}(sourcePath)`)({ sourcePath }));
-            }
-          },
-        });
-      },
+    CallExpression(path, state) {
+      if(path.node.callee.type == 'Import') {
+        path.node.callee.type = 'Identifier';
+        path.node.callee.name = (state.opts.by || '__import__');
+      }
     },
   },
 });
